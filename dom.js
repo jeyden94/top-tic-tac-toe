46a58ea04d0a4fefb6c04a023d06ea5c7ff0ce2e
jeyden94 <jeyden94@gmail.com>
2025-08-16 15:42:13 -0500

    Cleaned up comments, need to add win condition function.

--- dom.js
+++ dom.js
@@ -1,35 +1,7 @@
-let numbers = [
-    [1, 2], 
-    [3, 4], 
-    [5, 6, 7],
-];
-
-// let resultsEven = numbers.filter( (num) => { return num % 2 === 0 });
-// let resultsOdd = numbers.filter( (num) => {return num % 2 === 1 });
-
-// console.log(resultsEven);
-// console.log(resultsOdd);
-
-// let resultsMap = numbers.map ( 
-//     (num) => {
-//         return num.map(  
-//             (num) => {return num*2} 
-//         )     
-//     });
-
-// console.log(resultsMap);
-
-// the game //
-
-
 function Gameboard() {
-    // Initialize the board and the dimensions of the board.
-    // In our case, 3x3 will suffice for tic tac toe.
     rows = 3;
     columns = 3;
     board = [];
-
-    // Now, use a nested loop structure to create our board.
 
     for (let i = 0; i < rows; i++) {
         board[i] = [];
@@ -48,6 +20,7 @@ function Gameboard() {
             board[row][col].setValue(player);
         } else {
             console.log("Can't play your mark here!");
+            return;
         }
     }
 
@@ -60,24 +33,25 @@ function Gameboard() {
         console.log(printedBoard);
     }
 
+    const checkWinCondition = () => {
+        
+    }
+
     return {
         getBoard,
         addMarker,
         printBoard,
     }
-
 }
 
 function Cell() {
-    // Give each cell it's starting value
+
     let value = "";
 
-    // Add function to update value with an X or O
     const setValue = (player) => {
         value = player; 
     }
 
-    // Add function to retrive the value from this cell
     const getValue = () => value;
     
     return {
@@ -87,7 +61,6 @@ function Cell() {
 }
 
 function GameController(
-    // Initialize our players with default names
     playerOneName = "Player One",
     playerTwoName = "Player Two"
 ) {
@@ -118,18 +91,11 @@ function GameController(
     }
 
     const playRound = (col, row) => {
-
         let marker = getActivePlayer().marker;
-
         board.addMarker(col, row, marker);
-
         console.log(`${getActivePlayer().name} plays row ${row}, column ${col}.`);
-
-        board.printBoard();
-
         switchActivePlayer();
-
-        console.log(`${getActivePlayer().name}'s turn.`);
+        printNewRound();
     }
 
     return {
@@ -137,10 +103,5 @@ function GameController(
         playRound,
     }
 }
-
-
-// Put the game into a factory function for code cleanliness
-
-// const testGame = Gameboard();
 
 const game = GameController();
