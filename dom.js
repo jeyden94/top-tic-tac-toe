d505e64cddbf458147d39f07167b8d8dce3e823a
jeyden94 <jeyden94@gmail.com>
2025-08-16 18:10:27 -0500

    Win condition check functional

--- dom.js
+++ dom.js
@@ -2,25 +2,70 @@ function Gameboard() {
     rows = 3;
     columns = 3;
     board = [];
+    let cellCounter = 1;
 
     for (let i = 0; i < rows; i++) {
         board[i] = [];
         for (let j = 0; j < columns; j++) {
-            board[i].push(Cell());
+            board[i].push(Cell(cellCounter));
+
+            cellCounter++
         }
     }
 
     const getBoard = () => board;
 
-    const addMarker = (col, row, player) => {
+    const checkForWin = (arr) => {
+
+        let cellOne = arr[0];
+        let cellTwo = arr[1];
+        let cellThree = arr[2];
+
+        let xPoints = 0;
+        let oPoints = 0;
+
+        for (let i = 0; i < rows; i++) {
+
+            for (let j = 0; j < columns; j++) {
+
+                if (cellOne === board[i][j].getCellId() && board[i][j].getValue() === "X" ) {
+                    xPoints++;
+                }
+                if (cellTwo === board[i][j].getCellId() && board[i][j].getValue() === "X" ) {
+                    xPoints++;
+                }
+                if (cellThree === board[i][j].getCellId() && board[i][j].getValue() === "X" ) {
+                    xPoints++;
+                }
+
+                if (cellOne === board[i][j].getCellId() && board[i][j].getValue() === "O" ) {
+                    oPoints++;
+                }
+                if (cellTwo === board[i][j].getCellId() && board[i][j].getValue() === "O" ) {
+                    oPoints++;
+                }
+                if (cellThree === board[i][j].getCellId() && board[i][j].getValue() === "O" ) {
+                    oPoints++;
+                }
+            }
+        }  
+
+        if (xPoints === 3) {
+            return 2; // Return 2 for "X" Player Victory
+        } else if (oPoints === 3) {
+            return 1; // Return 1 for "O" Player Victory
+        } else {return 0;}
+
+    }
+
+    const addMarker = (row, col, player) => {
         row--;
         col--;
 
         if (board[row][col].getValue() === "") {
             board[row][col].setValue(player);
         } else {
-            console.log("Can't play your mark here!");
-            return;
+            throw Error("Can't place your mark here!");
         }
     }
 
@@ -33,18 +78,15 @@ function Gameboard() {
         console.log(printedBoard);
     }
 
-    const checkWinCondition = () => {
-        
-    }
-
     return {
         getBoard,
         addMarker,
         printBoard,
+        checkForWin,
     }
 }
 
-function Cell() {
+function Cell(cellId) {
 
     let value = "";
 
@@ -54,9 +96,12 @@ function Cell() {
 
     const getValue = () => value;
     
+    const getCellId = () => cellId;
+
     return {
         setValue,
         getValue,
+        getCellId,
     }
 }
 
@@ -64,6 +109,7 @@ function GameController(
     playerOneName = "Player One",
     playerTwoName = "Player Two"
 ) {
+    let round = 0;
 
     const players = [
         {
@@ -90,18 +136,93 @@ function GameController(
         console.log(`${getActivePlayer().name}'s turn.`);
     }
 
-    const playRound = (col, row) => {
+    const playRound = (row, col) => {
+               
         let marker = getActivePlayer().marker;
-        board.addMarker(col, row, marker);
+        board.addMarker(row, col, marker);
+        round++;
         console.log(`${getActivePlayer().name} plays row ${row}, column ${col}.`);
+        checkForEndGame();
+    }
+
+    printNewRound();
+
+    const checkForEndGame = () => {
+        let status = 0;
+
+        let winningCombinations = [
+            [1,2,3],
+            [4,5,6],
+            [7,8,9],
+            [1,4,7],
+            [2,5,8],
+            [3,6,9],
+            [1,5,9],
+            [7,5,3]
+        ]
+    
+        for (let i in winningCombinations) {
+            let status = board.checkForWin(winningCombinations[i]);
+      
+            if (status === 2) {
+                console.log(`${players[0].name} Wins!`)
+                board.printBoard();
+                return;
+            }
+        
+            if (status === 1) {
+                console.log(`${players[1].name} Wins!`)
+                board.printBoard();
+                return;
+            }
+        }
+
+        if (status === 0 && round === 9) {
+            console.log("It's a tie!")
+            board.printBoard();
+            return;
+        }
+
         switchActivePlayer();
         printNewRound();
+    
     }
 
     return {
         printNewRound,
         playRound,
+        checkForEndGame,
     }
 }
 
 const game = GameController();
+
+
+// Debugging -- Tie Scenario
+
+// game.playRound(1,1) // X Turn 1
+// game.playRound(1,2) // O Turn 1
+// game.playRound(1,3) // X Turn 2
+// game.playRound(2,2) // O Turn 2
+// game.playRound(2,1) // X Turn 3
+// game.playRound(2,3) // O Turn 3
+// game.playRound(3,3) // X Turn 4
+// game.playRound(3,1) // O Turn 4
+// game.playRound(3,2) // X Turn 5
+
+// Debugging -- X Win Scenario
+
+game.playRound(1,1) // X Turn 1
+game.playRound(1,2) // O Turn 1
+game.playRound(3,1) // X Turn 2
+game.playRound(2,2) // O Turn 2
+game.playRound(2,1) // X Turn 3
+
+// Debugging -- Y Win Scenario
+
+// game.playRound(1,1) // X Turn 1
+// game.playRound(1,2) // O Turn 1
+// game.playRound(3,1) // X Turn 2
+// game.playRound(2,2) // O Turn 2
+// game.playRound(2,3) // X Turn 3
+// game.playRound(3,2) // O Turn 2
