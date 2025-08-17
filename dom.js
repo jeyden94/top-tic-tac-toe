301da25de8089569c6b5a9672e009ff4e63dd2b7
jeyden94 <jeyden94@gmail.com>
2025-08-16 22:38:29 -0500

    ScreenController added, html added, basic css styling added.

--- dom.js
+++ dom.js
@@ -8,7 +8,6 @@ function Gameboard() {
         board[i] = [];
         for (let j = 0; j < columns; j++) {
             board[i].push(Cell(cellCounter));
-
             cellCounter++
         }
     }
@@ -192,11 +191,72 @@ function GameController(
         printNewRound,
         playRound,
         checkForEndGame,
+        getActivePlayer,
+        getBoard: board.getBoard,
     }
 }
 
-const game = GameController();
+// const game = GameController();
 
+function ScreenController() {
+    const game = GameController();
+    const playerTurnDiv = document.querySelector('.turn');
+    const boardDiv = document.querySelector('.board');
+
+    const updateScreen = () => {
+        // clear the board
+        boardDiv.textContent = "";
+
+        // get the newest version of the board and player turn
+        const board = game.getBoard();
+        const activePlayer = game.getActivePlayer();
+
+        // Display player's turn
+        playerTurnDiv.textContent = `${activePlayer.name}'s turn...`
+
+        // Render board squares
+        let counter = 0;
+        board.forEach(row => {
+        row.forEach((cell, index) => {
+            // Anything clickable should be a button!!
+            counter++;
+            const cellButton = document.createElement("button");
+            cellButton.classList.add("cell");
+            // Create a data attribute to identify the column
+            // This makes it easier to pass into our `playRound` function 
+            if (counter < 4) {
+                cellButton.dataset.row = 1;
+            } else if (counter < 7) {
+                cellButton.dataset.row = 2;
+            } else {cellButton.dataset.row = 3;}
+            cellButton.dataset.column = index + 1;
+            cellButton.textContent = cell.getValue();
+            boardDiv.appendChild(cellButton);
+        })
+        })
+
+    }
+
+    // Add event listener for the board
+    function clickHandlerBoard(e) {
+        const selectedColumn = e.target.dataset.column;
+        const selectedRow = e.target.dataset.row;
+        // Make sure I've clicked a column and not the gaps in between
+        if (!selectedColumn) return;
+        if (!selectedRow) return;
+        
+        game.playRound(selectedRow, selectedColumn);
+        updateScreen();
+    }
+    boardDiv.addEventListener("click", clickHandlerBoard);
+
+    // Initial render
+    updateScreen();
+
+    // We don't need to return anything from this module because everything is encapsulated inside this screen controller.
+}
+
+ScreenController();
 
 // Debugging -- Tie Scenario
 
@@ -212,11 +272,11 @@ const game = GameController();
 
 // Debugging -- X Win Scenario
 
-game.playRound(1,1) // X Turn 1
-game.playRound(1,2) // O Turn 1
-game.playRound(3,1) // X Turn 2
-game.playRound(2,2) // O Turn 2
-game.playRound(2,1) // X Turn 3
+// game.playRound(1,1) // X Turn 1
+// game.playRound(1,2) // O Turn 1
+// game.playRound(3,1) // X Turn 2
+// game.playRound(2,2) // O Turn 2
+// game.playRound(2,1) // X Turn 3
 
 // Debugging -- Y Win Scenario
 
