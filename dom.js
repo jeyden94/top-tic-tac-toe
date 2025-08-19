23f1d0f0c63bde1955ca41f9f5a618ebf3c9044d
jeyden94 <jeyden94@gmail.com>
2025-08-19 17:45:26 -0500

    Adding comments for all code

--- dom.js
+++ dom.js
@@ -69,12 +69,11 @@ function Gameboard() {
     }
 
     const printBoard = () => {
-        const printedBoard = board.map((row) => {
+        board.map((row) => {
             return row.map((column) => {
                 return column.getValue()
             })
         });
-        console.log(printedBoard);
     }
 
     return {
@@ -104,14 +103,18 @@ function Cell(cellId) {
     }
 }
 
-function GameController(
-    playerOneName = "Player One",
-    playerTwoName = "Player Two"
-) {
+function GameController(playerOneName, playerTwoName) {
+    // Create a round counter to help track for ties
     let round = 0; // One round for each square 1-9
-    let status = 0; // If 0, no winner or tie; If 1, Player 2 has won; If 2, Player 3 has won.
-    let winner = "hi";
+    
+    // Probably a better way to do this, but using a status to track win conditions
+    // If 0, no winner or tie; If 1, Player 2 has won; If 2, Player 3 has won.
+    let status = 0;
 
+    // Initialize our winner
+    let winner = "";
+
+    // Add players to an array to consolidate names and their markers
     const players = [
         {
             name: playerOneName,
@@ -123,21 +126,23 @@ function GameController(
         }
     ]
 
-    console.log(`${players[0].name}`)
-    console.log(`${players[1].name}`)
-
+    // Call the Gameboard function and stick the return values into a "board" variable
     const board = Gameboard();
 
+    // Initialize the active player and the first player in the array. Only runs when new game is launched
     let activePlayer = players[0];
 
+    // Create an active player switch which runs everytime a marker is set down. Swaps active player to non-active
     const switchActivePlayer = () => {
         activePlayer = activePlayer === players[0] ? players[1] : players[0];
     }
+
+    // Create function I can run in the game screen controller that allows me to grab active player
     const getActivePlayer = () => activePlayer;
 
+    // Create function I can run in the game screen controller that allows me to print the board
     const printNewRound = () => {
         board.printBoard();
-        console.log(`${getActivePlayer().name}'s turn.`);
     }
 
     const playRound = (row, col) => {
@@ -145,29 +150,19 @@ function GameController(
         let marker = getActivePlayer().marker;
         board.addMarker(row, col, marker);
         round++;
-        console.log(`${getActivePlayer().name} plays row ${row}, column ${col}.`);
         status = checkForEndGame(status);
 
-        console.log(round)
-        console.log(status)
-
         if (status === 2) {
-            console.log(`${players[0].name} Wins!`)
-            winner = `${players[0].name}`
             board.printBoard();
-            return winner;
+            return `${players[0].name}`;
         } else if (status === 1) {
-            console.log(`${players[1].name} Wins!`)
-            winner = `${players[1].name}`
             board.printBoard();
-            return winner;
+            return `${players[1].name}`;
         } else if (status === 0 && round === 9) {
-            winner = "tie"
-            console.log("It's a tie!")
             board.printBoard();
-            return winner;
+            return "tie";
         } else {
-            return winner;
+            return "";
         }
     }
 
@@ -221,45 +216,38 @@ function GameController(
         checkForEndGame,
         getActivePlayer,
         getBoard: board.getBoard,
-        // GameController,
     }
 }
 
 // const game = GameController();
 
-function GameScreenController(
-    playerOneName = "Player One",
-    playerTwoName = "Player Two"  
-) {
+function GameScreenController(playerOneName, playerTwoName) {
+
+    // Before initializing the game screen, call the GameController to launch the logic that behind the board
+    // Store this in a new game variable to access functions associated the game logic
     const game = GameController(playerOneName, playerTwoName);
 
+    // Create game container to replace menu container
+    const gameContainer = document.querySelector('.game-container');
+
+    // Create new child divs for the game container, add classes for styling
     const boardDiv = document.createElement("div")
     boardDiv.classList.add("board");
-    
     const playerTurnDiv = document.createElement("h1");
     playerTurnDiv.classList.add("turn");
-    
     const playAgainButton = document.createElement("button");
     playAgainButton.classList.add("play-again-btn");
 
-    const gameContainer = document.querySelector('.game-container');
-    
+    // Append new children, excluding the play again
     gameContainer.appendChild(playerTurnDiv);
     gameContainer.appendChild(boardDiv);
-    
-    console.log(game.winner)
-
-    if (game.winner === "tie") {
-        console.log("hello")
-    }
-    // const boardDivSelector = document.querySelector('.board');
 
     const updateScreen = (winner) => {
-        // clear the board
-       
+        
+        // Start by clearing the board
         boardDiv.textContent = "";
         
-        // get the newest version of the board and player turn
+        // Get the latest array of the board and the current player so we know whose turn it is
         const board = game.getBoard();
         const activePlayer = game.getActivePlayer();
 
@@ -267,36 +255,41 @@ function GameScreenController(
         playerTurnDiv.textContent = `${activePlayer.name}'s turn...`
 
         // Render board squares
-        let counter = 0;
+        let currentCell = 0; // Current cell tracker to assign row numbers and columns
         board.forEach(row => {
             row.forEach((cell, index) => {
-                // Anything clickable should be a button!!
-                counter++;
+                
+                // Increment cell tracker by 1
+                currentCell++;
+
+                // Create a new cell button for each cell in the grid and give it a class
                 const cellButton = document.createElement("button");
                 cellButton.classList.add("cell");
-                // Create a data attribute to identify the column
+
+                // Create data attributes to identify columns and rows
                 // This makes it easier to pass into our `playRound` function 
-                    if (counter < 4) {
-                        cellButton.dataset.row = 1;
-                    } else if (counter < 7) {
-                        cellButton.dataset.row = 2;
-                    } else {cellButton.dataset.row = 3;}
+                if (currentCell < 4) {
+                    cellButton.dataset.row = 1;
+                } else if (currentCell < 7) {
+                    cellButton.dataset.row = 2;
+                } else {cellButton.dataset.row = 3;}
+
                 cellButton.dataset.column = index + 1;
                 cellButton.textContent = cell.getValue();
+
+                // Stick the button onto the board, will use css to turn it into 3x3 grid
                 boardDiv.appendChild(cellButton);
+
             });
         });
-
-
-        if (winner === "tie") {
-            playerTurnDiv.textContent = "It's a tie!"
-            endGameFlow();
-        }
 
         if (winner === `${game.players[0].name}` || winner === `${game.players[1].name}`) {
             playerTurnDiv.textContent = `${winner} wins!`
             endGameFlow();
-        }
+        } else if (winner === "tie") {
+            playerTurnDiv.textContent = "It's a tie!"
+            endGameFlow();
+        } else return
        
     }
 
@@ -338,53 +331,67 @@ function GameScreenController(
 }
 
 function LaunchScreenController() {
-    
-
-
+    // Create menu container and its children.
     const menuContainer = document.querySelector(".menu-container");
-    
     const gameTitle = document.createElement("h1")
     const playerNameForm = document.createElement("form")
     const launchButton = document.createElement("button")
+
+    // Add class to the launch button for styling
     launchButton.classList.add("start-btn")
 
+    // Append children to the menu container
     menuContainer.appendChild(gameTitle);
     menuContainer.appendChild(playerNameForm);
     menuContainer.appendChild(launchButton);
 
+    // Create player one form label and input and set attributes of type and value, respectively
     const playerOneNameLabel = document.createElement("label")
     const playerOneNameInput = document.createElement("input")
     playerOneNameInput.setAttribute("type","text")
-    playerOneNameInput.setAttribute("value","Player One")
+    playerOneNameInput.setAttribute("placeholder","Player One")
 
+    // Create player two form label and input and set attributes of type and value, respectively
     const playerTwoNameLabel = document.createElement("label")
     const playerTwoNameInput = document.createElement("input")
     playerTwoNameInput.setAttribute("type","text")
-    playerTwoNameInput.setAttribute("value","Player Two")
+    playerTwoNameInput.setAttribute("placeholder","Player Two")
 
-    playerNameForm.appendChild(playerOneNameLabel);
-    playerNameForm.appendChild(playerOneNameInput);
-    playerNameForm.appendChild(playerTwoNameLabel);
-    playerNameForm.appendChild(playerTwoNameInput);
-
+    // Function which shows the menu by adding text content and appending form children
     const showMenu = () => {
         gameTitle.textContent = "Tic Tac Toe"
         playerOneNameLabel.textContent = "Player One Name"
         playerTwoNameLabel.textContent = "Player Two Name"
         launchButton.textContent = "Start Game"
+        
+        // Append form labels and inputs for both players to the form
+        playerNameForm.appendChild(playerOneNameLabel);
+        playerNameForm.appendChild(playerOneNameInput);
+        playerNameForm.appendChild(playerTwoNameLabel);
+        playerNameForm.appendChild(playerTwoNameInput);
     }
 
     // Add event listener for the start button
-    function clickHandlerStart(e) {
+    function clickHandlerStart() {
+        // Hide the menu
         hideMenu();
-        const playerOneName = playerOneNameInput.value;
-        const playerTwoName = playerTwoNameInput.value;
 
-        GameScreenController(playerOneName,playerTwoName);
+        // Check is player names have a value, if so, use it, if not, use the placeholder text
+        if (playerOneNameInput.value === "") {
+            playerOne = playerOneNameInput.placeholder;
+        } else (playerOne = playerOneNameInput.value)
+     
+        if (playerTwoNameInput.value === "") {
+            playerTwo = playerTwoNameInput.placeholder;
+        } else (playerOne = playerTwoNameInput.value)
+
+        // Launch the GameScreenController, pass values for the player names
+        GameScreenController(playerOne, playerTwo);
     }
     launchButton.addEventListener("click", clickHandlerStart);
 
     const hideMenu = () => {
+        // Remove children from the menu container to hide it upon game launch
         menuContainer.removeChild(gameTitle);
         menuContainer.removeChild(playerNameForm);
         menuContainer.removeChild(launchButton);    
@@ -392,8 +399,6 @@ function LaunchScreenController() {
 
     // Initial render
     showMenu();
-
-    // We don't need to return anything from this module because everything is encapsulated inside this screen controller.
 }
 
 LaunchScreenController();
